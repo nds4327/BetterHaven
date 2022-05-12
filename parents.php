68a21c3e0207d79b9e98d51b8be5f1ae55936c23
nds4327 <104733517+nds4327@users.noreply.github.com>
2022-05-12 11:13:23 -0400

    Add files via upload

---
+++ parents.php
@@ -0,0 +1,50 @@
+<?php
+include_once 'header.php';
+include_once 'session.php';
+
+?>
+
+
+    <div class= "about">
+        <h2>About This Page</h2>
+        <p>Welcome to BetterHaven! This page provides quick access to pages that are commonly accessed by parents. If you need additional assistance, please call us at 570-484-2011</p>
+    </div>
+
+    <div>
+        <h3>Quick Links</h3>
+        <table>
+            <tr>
+                <td><a href="https://lockhaven.university-tour.com" class= "parents">Campus Map</a></td>
+                <td><a href="https://www.lockhaven.edu/career/" class= "parents">Center for Career and Professional Development</a></td>
+                <td><a href="https://lockhaven.campusdish.com" class= "parents">Dining Services</a></td>
+                <td><a href="https://www.lockhaven.edu/admissions/visit/directions.html" class= "parents">Directions to LHU</a></td>
+            </tr>
+            <tr>
+                <td><a href="https://www.lockhaven.edu/financialaid/" class= "parents">Financial Aid</a></td>
+                <td><a href="https://www.lockhaven.edu/healthservices/" class= "parents">Health Services</a></td>
+                <td><a href="https://www.lockhaven.edu/publicsafety/parking.html" class= "parents">Parking</a></td>
+                <td><a href="https://www.lockhaven.edu/publicsafety/" class= "parents">Public Safety</a></td>
+            </tr>
+            <tr>
+                <td>Registrar</td>
+                <td><a href="https://www.lockhaven.edu/scholarships/" class= "parents">Scholarships</a></td>
+                <td><a href="https://www.lockhaven.edu/studentaccounts/" class= "parents">Student Accounts</a></td>
+                <td><a href="https://www.lockhaven.edu/housing/" class= "parents">Student Housing</a></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><a href="https://eacct-havencard-sp.transactcampus.com/eaccounts/AnonymousHome.aspx" class= "parents">Add Haven Dollars</a></td>
+                <td><a href="https://eacct-havencard-sp.transactcampus.com/havendollars/AnonymousHome.aspx" class= "parents">Add Flex Dollars</a></td>
+                <td></td>
+            </tr>
+        </table>
+    </div>
+
+
+
+<?php
+
+include_once 'footer.php';
+
+
+?>
---
+++ registrar.php
@@ -0,0 +1,128 @@
+<?php 
+
+include_once 'header.php';
+include_once 'session.php';
+
+
+?>
+
+<main>
+    <div class="rQuickCols">
+        <div class="rContact">
+            <h3>Office Staff</h3>
+            <p><a href="mailto:mmcdermi@lockhaven.edu" class= "email">Meisha McDermit</a>, Interim Registrar</p>
+            <p><a href="mailto:tina.probst@lockhaven.edu" class= "email">Tina Probst</a>, Clerical Supervisor </p>
+            <p><a href="mailto:kjl802@lockhaven.edu" class= "email">Kylie Shirk</a>Kylie Shirk, Administrative Secretary </p>
+            <p><a href="mailto:hmp8036@lockhaven.edu" class= "email">Hannah Plotts</a>, Administrative Secretary </p>
+        </div>
+        <div class="rQuick">
+            <h3>Contact Information</h3>
+            <p>Office Hours:  Monday through Friday, 8:00 AM - 4:00 PM</p>
+            <p>Location:  Ulmer 224</p>
+            <p>Fax:  570-484-2734</p>
+            <p>Email:  registrar@lockhaven.edu</p>
+        </div>
+    </div>
+
+    <div class="about">
+        <h2 id="anchorOne">Welcome</h2>
+        <p>The Registrar's Office is responsible for student scheduling and registration, processing grades, graduation clearance, verifying enrollment and degree, maintaining the student's official academic record, transfer credit evaluations, and administering academic policies.</p>
+        <h3>Mission Statement</h3>
+        <p>The Registrar's Office is dedicated to providing quality service in an efficient, professional, and ethical manner.  The members of the office strive to uphold the policies of the University and to preserve the academic integrity of the University.  The Registrar's Office staff offer helpful assistance and accurate and prompt responses.  We create a welcome atmosphere by treating each individual in a professional and respectful manner.</p>
+        <h3>Office Feedback</h3>
+        <p>If you have contacted the Registrar's Office and would like to commend an employee for professional and helpful service or if you feel you were provided incorrect information or not treated professionally and respectfully, I would appreciate hearing from you.  Please email me with details.</p>
+        <p>Meisha McDermit, LHU Interim Registrar</p>
+    </div>
+
+    <div class="rAnchors">
+        <h3>Quick Jump</h3>
+        <p><a href="#anchorOne" class="anchor">Welcome</a></p>
+        <p><a href="#anchorTwo" class="anchor">Important Information</a></p>
+        <p><a href="#anchorThree" class="anchor">Grades</a></p>
+        <p><a href="#anchorFour" class="anchor">New Students</a></p>
+        <p><a href="#anchorFive" class="anchor">Course Registration</a></p>
+        <p><a href="#anchorSix" class="anchor">Withdrawl/Not Returning</a></p>
+        <p><a href="#anchorSeven" class="anchor">Medical Withdraw</a></p>
+        <p><a href="#anchorEight" class="anchor">Not Returning/Transfer</a></p>
+        <p><a href="#anchorNine" class="anchor">Withdraw or Drop a Single Course at LHU</a></p>
+        <p><a href="#anchorTen" class="anchor">Special Class Schedules</a></p>
+    </div>
+
+    <div class="Registrar-Imp">
+        <h2 id="anchorTwo">Important Information</h2>
+        <h4 id="anchorThree">Grades</h4>
+            <ul>
+                <li>Grades are not mailed. Students log on to their BetterHaven account to access grades.</li>
+                <li>If you are placed on academic warning, probation, suspension, or dismissal at the close of the semester, your letter is mailed to your home address on record.</li>
+                <li>If you receive an incomplete grade and do not know why this grade has been recorded, please contact the instructor.</li>
+            </ul>
+        <h4 id="anchorFour">New Students</a></h4>
+            <ul>
+                <li>Your schedule will be developed by the Registrar's Office</li>
+                <li>Schedule changes may occur; however, every effort will be made to keep changes to a minimum and done only if necessary.</li>
+                <li>New freshmen students may not make schedule changes.  New freshmen must contact their academic advisor or major department chairperson to discuss their schedule; the advisor or major department chairperson will make appropriate changes.</li>
+                <li>New transfer students are able to make schedule changes on their own; however, it is recommended to speak with your academic advisor or major department chairperson about the changes you wish to make.</li>
+                <li>If you purchase your textbooks prior to the start of the semester, please beware of the seller's return/refund policy in the event your schedule does change.  We encourage you to purchase your textbooks from the LHU Bookstore.</li>
+                <li>New freshmen will meet with their advisor to make schedule changes.</li>
+                <p>*STUDENTS ARE NOT PERMITTED TO “SIT IN” ON A CLASS WITHOUT BEING REGISTERED,  EVEN IF THEY ARE WAITING FOR A SEAT TO BECOME AVAILABLE. STUDENTS MUST BE OFFICIALLY REGISTERED BY THE “LAST DAY TO ADD” ON THE ACADEMIC CALENDAR.</p>
+            </ul>
+        <h3 id="anchorFive">Course Registration</h3>
+        <h4 id="anchorSix">Withdrawl/Not Returning</h4>
+            <ul>
+                <p>Please complete the Withdrawal/Not Returning form to notify LHU that you wish to complete one of the following actions:</p>
+                <li>CANCEL all registered classes</li>
+                <li>WITHDRAW from the current semester</li>
+                <li>COMPLETE A MEDICAL WITHDRAW from the current semester</li>
+                <li>NOT RETURN / TRANSFER</li>
+            </ul>
+            <h5>Complete the following setps if you would like to cancel registered courses for the upcoming semester (E. Complete in Spring for Fall or complete in Fall for Spring) </h5>
+            <ul>
+                <li>This cancels all future registrations for courses, rooms and meals, including summer sessions and winter intersession.</li>
+                <li>To return to LHU, students will complete the Request to Resume Studies form.</li>
+                <li>This is not a transcript request.  You may request transcripts by completing the LHU Transcript Request form. </li>
+            </ul>
+            <h5>Complete the following steps to withdraw from the current semester. This must be completed before or during the first week of the semester.</h5>
+            <p>*This option can be completed after you have accepted our offer of admission but before you have arrived to campus for the semester.</p>
+            <ul>
+                <li>You will not receive a refund of your deposit.</li>
+                <li>This does not remove your acceptance to LHU.  You may change your starting semester to the next semester if interested.</li>
+            </ul>
+            <h5>Complete the following steps to withdraw from the current semester. This can be completed during the current semester through the 10th week.</h5>
+            <ul>
+                <li>W grades are assigned during the first 10 weeks of the semester.</li>
+                <li>Students are responsible for tuition and fees according to the Student Accounts Refund Chart.</li>
+            </ul>
+            <h4 id="anchorSeven">Complete a Medical Withdraw (Current Semester)</h4>
+            <p>This option is available for students who have a medical situation preventing them from successfully completing the semester.  This type of withdrawal requires a Medical Verification Form to be submitted by a medical professional. Medical withdrawals are available for 30 days after the semester ends.</p>
+            <h4 id="anchorEight">Not Returning/Transfer</h4>
+            <p>This option is available for students who are transferring or not returning to LHU for whatever reason.  If you are not currently registered for classes and would like to let us know you are transferring to not returning to LHU.</p>
+            <h4 id="anchorNine">Withdraw or Drop a Single Course at LHU</h4>
+            <p>Complete the Add/Drop Process on BetterHaven</p>
+            <p>Students can drop or withdraw from their own courses until the 6th week of classes.</p>
+            <ul>
+                <li>First semester for freshman - all schedule changes are done with advisor or major department chair.</li>
+                <li>Faculty must withdraw students from individual courses beginning with the 6 th week and continuing through the 10th week of the fall and spring semesters. </li>
+                <p>*Note: Students will Add/Drop/Withdraw themselves during the Summer and Winter intersessions.</p>
+            </ul>
+        <h4 id="anchorTen">Special Class Schedules</h4>
+        <p>Special class schedules can be found <a href="https://www.lockhaven.edu/academics/calendar.html">here</a>.</p>
+        <p>Click <a href="https://www.lockhaven.edu/academics/calendar.html#regular">here</a> to see the regular class schedule.</p>
+        <p>Click <a href="https://www.lockhaven.edu/academics/calendar.html#delay">here</a> to see the inclement weather compressed schedule.</p>
+        </table>
+    </div>
+
+    <div class="rAbout">
+        <p>General Registrar Office questions, general registration questions, walk-in registrations, high school student registrations, substitution/waiver forms, transfer credits:  Phone - 570-484-2006</p>
+        <p>Enrollment verifications, major changes, advisor changes, address changes request to resume studies, new student advisor assignments, Act 48, registration of internships/individualized instructions/independent studies:  Phone - 570-484-2124</p>
+        <p>Master schedule of classes, room assignments (for credit-bearing courses), graduation:   Phone - 570-484-2008</p>
+        <p>If you are unsure which number to call or your reason for contacting the Registrar's Office is not included above, phone 570-484-2006. </p>
+        <p>Federal School Code:  003323</p>
+        <p>CEEB:  2654</p>
+    </div>
+</main>
+
+
+
+<?php
+
+include_once 'footer.php';
